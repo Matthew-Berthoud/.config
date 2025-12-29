dfcbec046d004c0811e84e9d5d82fb7af3d36764
Matthew Berthoud <matthewberthoud@gmail.com>
2025-12-29 15:13:27 -0600

    sort

--- nvim/init.lua
+++ nvim/init.lua
@@ -59,16 +59,16 @@ require("gitsigns").setup({
 	end,
 })
 
+vim.g.mapleader = " "
+vim.o.clipboard = 'unnamedplus'
 vim.o.number = true
 vim.o.relativenumber = true
-vim.o.wrap = false
-vim.o.signcolumn = "yes"
-vim.o.tabstop = 2
 vim.o.shiftwidth = 2
+vim.o.signcolumn = "yes"
 vim.o.swapfile = false
-vim.o.clipboard = 'unnamedplus'
-vim.g.mapleader = " "
+vim.o.tabstop = 2
 vim.o.winborder = "rounded"
+vim.o.wrap = false
 
 vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
 vim.keymap.set('n', '<leader>w', ':write<CR>')
