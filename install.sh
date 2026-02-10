f5f6b70cd9c18f3a6180023827f17c86d9186fb6
Matthew Berthoud <matthewberthoud@gmail.com>
2026-02-10 11:25:25 -0500

    get rid of weird autolist behavior

--- install.sh
+++ install.sh
@@ -10,6 +10,3 @@ ln -sf "$CONFIG/git/.gitconfig" "$HOME/.gitconfig"
 ln -sf "$CONFIG/git/.gitconfig-work" "$HOME/.gitconfig-work"
 
 brew bundle --clean
-
-source "$SCRIPTS/fnm_npm.sh"
-source "$SCRIPTS/rustup_cargo_rust.sh"
--- nvim/init.lua
+++ nvim/init.lua
@@ -192,7 +192,6 @@ require('render-markdown').setup({
   completions = { lsp = { enabled = true } },
   code = { border = 'thin' },
 })
-require('autolist').setup()
 
 require('oil').setup({
   lsp_file_methods = {
@@ -246,28 +245,36 @@ vim.o.winborder = 'rounded'
 
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
-vim.keymap.set('i', '<tab>', '<cmd>AutolistTab<cr>')
-vim.keymap.set('i', '<s-tab>', '<cmd>AutolistShiftTab<cr>')
--- vim.keymap.set("i", "<c-t>", "<c-t><cmd>AutolistRecalculate<cr>") -- an example of using <c-t> to indent
-vim.keymap.set('i', '<CR>', '<CR><cmd>AutolistNewBullet<cr>')
-vim.keymap.set('n', 'o', 'o<cmd>AutolistNewBullet<cr>')
-vim.keymap.set('n', 'O', 'O<cmd>AutolistNewBulletBefore<cr>')
-vim.keymap.set('n', '<CR>', '<cmd>AutolistToggleCheckbox<cr><CR>')
-vim.keymap.set('n', '<C-r>', '<cmd>AutolistRecalculate<cr>')
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'markdown', 'text', 'tex', 'plaintex', 'norg' },
+  callback = function()
+    require('autolist').setup()
 
--- cycle list types with dot-repeat
-vim.keymap.set('n', '<leader>cn', require('autolist').cycle_next_dr, { expr = true })
-vim.keymap.set('n', '<leader>cp', require('autolist').cycle_prev_dr, { expr = true })
+    -- vim.keymap.set('i', '<tab>', '<cmd>AutolistTab<cr>')
+    vim.keymap.set('i', '<s-tab>', '<cmd>AutolistShiftTab<cr>')
+    -- vim.keymap.set("i", "<c-t>", "<c-t><cmd>AutolistRecalculate<cr>") -- an example of using <c-t> to indent
+    vim.keymap.set('i', '<CR>', '<CR><cmd>AutolistNewBullet<cr>')
+    vim.keymap.set('n', 'o', 'o<cmd>AutolistNewBullet<cr>')
+    vim.keymap.set('n', 'O', 'O<cmd>AutolistNewBulletBefore<cr>')
+    vim.keymap.set('n', '<CR>', '<cmd>AutolistToggleCheckbox<cr><CR>')
+    -- vim.keymap.set('n', '<C-r>', '<cmd>AutolistRecalculate<cr>')
 
--- if you don't want dot-repeat
--- vim.keymap.set("n", "<leader>cn", "<cmd>AutolistCycleNext<cr>")
--- vim.keymap.set("n", "<leader>cp", "<cmd>AutolistCycleNext<cr>")
+    -- cycle list types with dot-repeat
+    vim.keymap.set('n', '<leader>cn', require('autolist').cycle_next_dr, { expr = true })
+    vim.keymap.set('n', '<leader>cp', require('autolist').cycle_prev_dr, { expr = true })
 
--- functions to recalculate list on edit
-vim.keymap.set('n', '>>', '>><cmd>AutolistRecalculate<cr>')
-vim.keymap.set('n', '<<', '<<<cmd>AutolistRecalculate<cr>')
-vim.keymap.set('n', 'dd', 'dd<cmd>AutolistRecalculate<cr>')
-vim.keymap.set('v', 'd', 'd<cmd>AutolistRecalculate<cr>')
+    -- if you don't want dot-repeat
+    -- vim.keymap.set("n", "<leader>cn", "<cmd>AutolistCycleNext<cr>")
+    -- vim.keymap.set("n", "<leader>cp", "<cmd>AutolistCycleNext<cr>")
+
+    -- functions to recalculate list on edit
+    vim.keymap.set('n', '>>', '>><cmd>AutolistRecalculate<cr>')
+    vim.keymap.set('n', '<<', '<<<cmd>AutolistRecalculate<cr>')
+    vim.keymap.set('n', 'dd', 'dd<cmd>AutolistRecalculate<cr>')
+    vim.keymap.set('v', 'd', 'd<cmd>AutolistRecalculate<cr>')
+  end,
+})
+
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set(
   'n',
