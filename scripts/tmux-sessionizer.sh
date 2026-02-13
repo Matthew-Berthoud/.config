3d93d29648082ce7530a01bac826d92b57847ba9
Matthew Berthoud <matthewberthoud@gmail.com>
2026-02-12 19:56:05 -0500

    added the window defaults to tmux sessionizer

--- scripts/tmux-sessionizer.sh
+++ scripts/tmux-sessionizer.sh
@@ -1,28 +1,48 @@
 #!/usr/bin/env zsh
 
 if [[ $# -eq 1 ]]; then
-  selected=${1:A}
+  directory=${1:A}
 else
   repos_dirs=($REPOS/*(N/D))
   personal_repos=(${repos_dirs:#*/black-cape})
   work_repos=($WORK_REPOS/*/*(N/D))
-  config="$HOME/.config"
-  selected=$(print -l $config $work_repos $personal_repos | fzf --delimiter / --with-nth -1)
+  directory=$(print -l $CONFIG $work_repos $personal_repos | fzf --delimiter / --with-nth -1)
 fi
 
-if [[ -z $selected ]]; then
+if [[ -z $directory ]]; then
   exit 0
 fi
 
-selected_name=$(basename "$selected" | tr . _)
+session_name=$(basename "$directory" | tr . _)
 
 if [[ -z $TMUX ]]; then
-  tmux new-session -A -s "$selected_name" -c "$selected"
+  tmux new-session -A -s "$session_name" -c "$directory"
   exit 0
 fi
 
-if ! tmux has-session -t="$selected_name" 2>/dev/null; then
-  tmux new-session -ds "$selected_name" -c "$selected"
+if ! tmux has-session -t="$session_name" 2>/dev/null; then
+  tmux new-session -ds "$session_name" -n "editor" -c "$directory"
+  tmux send-keys -t ${session_name}:editor "cd $directory" C-m
+  tmux send-keys -t ${session_name}:editor "nvim ." C-m
+
+  tmux new-window -t $session_name -n "terminal" -c "$directory"
+  tmux send-keys -t ${session_name}:terminal "git status" C-m
+
+  tmux new-window -t $session_name -n "server" -c "$directory"
+
+  tmux new-window -t $session_name -n "notes" -c "$directory"
+  # Open notes directory with same path relative to $NOTES as the repo is relative to $REPOS
+  relative_path="${directory#$REPOS/}"
+  echo $relative_path
+  # Config isn't in $REPOS so its notes are just in $NOTES/config
+  if [[ $directory == $CONFIG ]]; then
+    tmux send-keys -t ${session_name}:notes "cd $NOTES/config" C-m
+  else
+    tmux send-keys -t ${session_name}:notes "cd $NOTES/$relative_path" C-m
+  fi
+  # tmux send-keys -t ${session_name}:notes "daily" C-m
+  #
+  tmux select-window -t ${session_name}:terminal
 fi
 
-tmux switch-client -t "$selected_name"
+tmux switch-client -t "$session_name"
