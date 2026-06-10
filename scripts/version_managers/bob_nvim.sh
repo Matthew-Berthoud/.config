23cc740a22477c1c3058735220341ffb84e2f434
Matthew Berthoud <matthewberthoud@gmail.com>
2026-06-10 19:40:58 -0400

    actually use nightly neovim

--- scripts/version_managers/bob_nvim.sh
+++ scripts/version_managers/bob_nvim.sh
@@ -1,5 +1,1 @@
-# Only update if we aren't on nightly or if we want to force an update
-if ! nvim --version | grep -q "dev"; then
-    echo "Switching to Neovim nightly..."
-    yes | bob use nightly
-fi
+yes | bob use nightly
